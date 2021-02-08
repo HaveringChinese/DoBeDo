939abce759be9d4ee11f950e9b39b027ee5be0ce
HaveringChinese <nrconrad13@gmail.com>
2021-02-07 16:50:11 -0800

    add server and node dependencies

--- dbd.js
+++ dbd.js
@@ -1,8 +1,24 @@
-window.onload.alert("test!")
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+//app.use.bodyParser.urlencoded(url, extended: {true});
+
+app.get("/", function(req, res) {
+    res.send("Hello");
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000.");
+});
+
+
+window.onload.alert("test!");
 
 
 /*document.addEventListener("load", function () {
     var cyberPunk = new Audio("audios/")
     cyberPunk.play();
-}
-    
+}*/
