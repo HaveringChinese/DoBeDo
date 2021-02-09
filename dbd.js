430a9768097ad785a02ec46e57ce9f9543ddf47d
HaveringChinese <nrconrad13@gmail.com>
2021-02-08 18:05:34 -0800

    add embedded javascript

--- dbd.js
+++ dbd.js
@@ -3,19 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use("view engine", "ejs");
+
 
 //app.use.bodyParser.urlencoded(url, extended: {true});
 
+
 app.get("/", function(req, res) {
-    res.send("Hello");
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.write("<h1>Yay it's the weekend!</h1>");
+        res.write(currentDay);
+
+    }   else {
+        res.write(currentDay);
+        res.send("<h1>Boo! I have to work!</h1>");
+    }   
 });
 
 app.listen(3000, function() {
     console.log("Server started on port 3000.");
 });
-
-
-window.onload.alert("test!");
 
 
 /*document.addEventListener("load", function () {
